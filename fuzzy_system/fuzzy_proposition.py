ce38d2e010daafa38840da69295ae7e590cb6019
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2020-11-24 18:44:09 -0500

    [norms-conorms] take to a new file norms and conorms

--- fuzzy_system/fuzzy_proposition.py
+++ fuzzy_system/fuzzy_proposition.py
@@ -1,16 +1,5 @@
 from fuzzy_system.linguistic_variable import LinguisticVariable
-
-
-def tconorm_max(val1, val2): return max(val1, val2)
-
-
-def tconorm_product(val1, val2): return (val1 + val2) - val1 * val2
-
-
-def tnorm_min(val1, val2): return min(val1, val2)
-
-
-def tnorm_product(val1, val2): return val1 * val2
+from fuzzy_system.norms import *
 
 
 class FuzzyProposition:
