dfcde05c405216b6ca17bf60124d28f8b64b9822
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2020-11-24 13:21:18 -0500

    [preposition] add tconorm and tnorms

---
+++ fuzzy_system/fuzzy_proposition.py
@@ -0,0 +1,12 @@
+
+def tconorm_max(val1, val2): return max(val1, val2)
+
+
+def tconorm_product(val1, val2): return (val1 + val2) - val1 * val2
+
+
+def tnorm_min(val1, val2): return min(val1, val2)
+
+
+def tnorm_product(val1, val2): return val1 * val2
+
