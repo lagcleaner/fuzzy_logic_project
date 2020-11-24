32193f04757ee4118d20a857fdafd17c6d2364be
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2020-11-24 18:59:50 -0500

    [fuzzy-system] inference system controller

---
+++ fuzzy_system/fuzzy_system_impl.py
@@ -0,0 +1,22 @@
+from fuzzy_system.fuzzy_set import UnionFuzzySet
+from fuzzy_system.dediffusion import centroid_dediffusion, bisection_dediffusion, mom_dediffusion, lom_dediffusion, som_dediffusion
+
+
+class FuzzySystem:
+    def __init__(self, rules: list):
+        self.rules = rules
+
+    def infer(self, values: dict, dediffusion_method=centroid_dediffusion):
+        _, final_sets = self.rules[0].evaluate(values)
+        for rule in self.rules[1:]:
+            _, for_update = rule.eval(values)
+            for var_name in for_update:
+                final_sets[var_name] = UnionFuzzySet(
+                    final_sets[var_name],
+                    for_update[var_name]
+                )
+
+        return {
+            var_name: dediffusion_method(cfs)
+            for var_name, cfs in final_sets.items()
+        }
