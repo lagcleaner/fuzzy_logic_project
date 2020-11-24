d7386f7a6922ae4ec3e4e6452d2af2586730c3d8
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2020-11-23 23:07:27 -0500

    fuzzy set implementation

---
+++ fuzzy_system/fuzzy_set.py
@@ -0,0 +1,98 @@
+
+from fuzzy_system.membership_functions import *
+
+
+class AbstractFuzzySet:
+    '''
+    Abstract class for a fuzzy set representation, only needs an `membership` method.
+    '''
+
+    def membership(self, x):
+        raise NotImplementedError
+
+# Custom FuzzySet
+
+
+class CustomizableFuzzySet(AbstractFuzzySet):
+    '''
+    Generic fuzzy set with a domain and a membership function to be specified.
+    '''
+
+    def __init__(self, name: str, domain: tuple, membership_function=None):
+        if membership_function is None:
+            raise ValueError(
+                'Membership function cant be None'
+            )
+        self.name = name
+        self._membership = membership_function
+        self.domain = domain
+
+    def membership(self, x: float):
+        return self._membership(x)
+
+# Specific implementations
+
+
+class GammaFuzzySet(CustomizableFuzzySet):
+    '''
+    Fuzzy Set whit the gamma function as membership function.
+    '''
+
+    def __init__(self, name: str, domain: tuple = (10, 20), a=13, b=17):
+        if None in (name, a, b, domain) or len(domain) != 2:
+            raise ValueError
+        super().__init__(
+            name,
+            domain,
+            membership_function=gammamf(a, b)
+        )
+
+
+class LambdaFuzzySet(CustomizableFuzzySet):
+    '''
+    Fuzzy Set whit the lambda function as membership function.
+    '''
+
+    def __init__(self, name: str, domain: tuple = (10, 20), a=13, b=17, m=15):
+        if None in (name, a, b, m, domain) or len(domain) != 2:
+            raise ValueError
+        super().__init__(
+            name,
+            domain,
+            membership_function=lmbmf(a, b, m)
+        )
+
+
+class TrapezoidalFuzzySet(CustomizableFuzzySet):
+    '''
+    Fuzzy Set whit the triangle function as membership function.
+    '''
+
+    def __init__(self, name: str, domain: tuple = (10, 20), a=12, b=14, c=16, d=18):
+        if None in (name, a, b, c, d, domain) or len(domain) != 2:
+            raise ValueError
+        super().__init__(
+            name,
+            domain,
+            membership_function=trapmf(a, b, c, d)
+        )
+
+
+class SigmoidalFuzzySet(CustomizableFuzzySet):
+    '''
+    Fuzzy Set whit the triangle function as membership function.
+    '''
+
+    def __init__(self, name: str, domain: tuple = (10, 20), a=12, b=18, middle=15):
+        if None in (name, a, b, domain) or len(domain) != 2:
+            raise ValueError
+        super().__init__(
+            name,
+            domain,
+            membership_function=smf(a, b, middle=middle)
+        )
+
+
+# aliases
+PiFuzzySet = TrapezoidalFuzzySet
+TriangleFuzzySet = LambdaFuzzySet
