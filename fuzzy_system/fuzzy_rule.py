6b92c30adb184a896b8f4e2c138bb0d1896df6c2
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2020-11-24 18:58:26 -0500

    [fuzzy-rule] add fuzzy rule and respective implementations for mamdani and larsen

---
+++ fuzzy_system/fuzzy_rule.py
@@ -0,0 +1,46 @@
+from fuzzy_system.fuzzy_proposition import FuzzyProposition
+from fuzzy_system.fuzzy_set import FuzzySet, MamdaniCut, LarsenScale
+
+# FuzzyConsequent = type(tuple(_, _))
+
+
+class FuzzyRule:
+    '''
+    A General Rule for apply any method of inference.
+    '''
+
+    def __init__(self, preposition: FuzzyProposition, consequent: list, method_transformer=None):
+        self.transformer = method_transformer
+        self.preposition = preposition
+        self.consequent = consequent
+
+    def evaluate(self, values: dict):
+        prepos_value: float = self.preposition.evaluate(values)
+        conseq_transformed_sets = {}
+        if not (self.transformer is None):
+            for variable, descrip in self.consequent:
+                fs = variable.descriptors[descrip]
+                conseq_transformed_sets[variable.name] = \
+                    self.transformer(
+                    prepos_value,
+                    fs
+                )
+        return prepos_value, conseq_transformed_sets
+
+
+class MamdaniRule(FuzzyRule):
+    def __init__(self, preposition: FuzzyProposition, consequent: list):
+        super().__init__(
+            preposition,
+            consequent,
+            method_transformer=lambda val, fs: MamdaniCut(fs, val)
+        )
+
+
+class LarsenRule(FuzzyRule):
+    def __init__(self, preposition: FuzzyProposition, consequent: list):
+        super().__init__(
+            preposition,
+            consequent,
+            method_transformer=lambda val, fs: LarsenScale(fs, val)
+        )
